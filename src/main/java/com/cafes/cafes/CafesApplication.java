f5e8e5545604d9217a9b6da59ab504f9b1a32ec3
Yurii <yurij.kenu@gmail.com>
2025-11-25 16:44:00 +0100

    initial-api: added initial spring-boot

---
+++ src/main/java/com/cafes/cafes/CafesApplication.java
@@ -0,0 +1,13 @@
+package com.cafes.cafes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafesApplication.class, args);
+	}
+
+}
