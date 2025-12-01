ea4cc43c97836fcde96ab5601ab6a6c5dfdf5b90
Yurii Keniu <kenu21@gmail.com>
2025-12-01 09:03:09 +0100

    intial-api: added structure and cafes endpoint

---
+++ src/main/java/com/cafes/cafes/entities/CityEntity.java
@@ -0,0 +1,31 @@
+package com.cafes.cafes.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cities")
+public class CityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/cafes/cafes/services/CafeService.java
@@ -0,0 +1,25 @@
+package com.cafes.cafes.services;
+
+import com.cafes.cafes.CafeDtoResponse;
+import com.cafes.cafes.mappers.CafeMapper;
+import com.cafes.cafes.repositories.CafeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CafeService {
+    private final CafeRepository cafeRepository;
+    private final CafeMapper cafeMapper;
+
+    @Autowired
+    public CafeService(CafeRepository cafeRepository, CafeMapper cafeMapper) {
+        this.cafeRepository = cafeRepository;
+        this.cafeMapper = cafeMapper;
+    }
+
+    public Page<CafeDtoResponse> getCafes(Pageable pageable) {
+        return cafeRepository.findAll(pageable).map(cafeMapper::toDto);
+    }
+}
