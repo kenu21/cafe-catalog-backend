057107c62f5377e40f2bdec1bfe742a835630e8c
Yurii <yurij.kenu@gmail.com>
2025-12-05 20:56:22 +0100

    refactor: refactored

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Initial
+# Local Backend Setup
+
+Before running the backend locally, make sure to clone the database data repository:
+
+```bash
+git clone git@github.com:kenu21/cafe-catalog-data.git
+```
+
+To run the backend locally, please create a `.env` file in the root of the project. It should look like this:
+
+```env
+MYSQL_DATABASE=coffee_shop
+MYSQL_ROOT_PASSWORD=
+MYSQL_USER=
+MYSQL_PASSWORD=
+MYSQL_DATABASE_URL="jdbc:mysql://db:${MYSQL_CONTAINER_PORT}/${MYSQL_DATABASE}?allowPublicKeyRetrieval=true&useSSL=false&allowLoadLocalInfile=true&allowLocalInfile=true"
+
+MYSQL_LOCAL_PORT=3307
+MYSQL_CONTAINER_PORT=3306
+
+BACKEND_LOCAL_PORT=8081
+BACKEND_CONTAINER_PORT=80
+
+FRONTEND_LOCAL_PORT=81
+
+COMPOSE_EXTENDS_PATH=
+```
+
+Fill in the required values in the file.
+
+Next, in the root of the project, run:
+
+```bash
+docker compose up -d
+```
+
+After that, you can make queries like this:
+
+```bash
+http://localhost:8081/api/v1/cafes
+```
--- src/main/java/com/cafes/cafes/configs/WebConfig.java
+++ src/main/java/com/cafes/cafes/configs/WebConfig.java
@@ -1,5 +1,6 @@
 package com.cafes.cafes.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${FRONTEND_LOCAL_PORT:81}")
+    private String frontendPort;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                String frontendOrigin = "http://localhost:" + frontendPort;
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:81")
+                        .allowedOrigins(frontendOrigin)
                         .allowedMethods("*")
                         .allowCredentials(true);
             }
